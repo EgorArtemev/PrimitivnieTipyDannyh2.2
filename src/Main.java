4cb213335798a267b2ac1abe432856268a11f18d
artemev <kik2007@mail.ru>
2023-07-17 01:24:12 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 100;
+        int recharge1 = 300;
+        int recharge2 = 1100;
+        int summary1 = recharge1 + balance;
+        int summary2 = recharge2 + balance;
+        int bonus1 = summary1 / 100;
+        int bonus2 = summary2 / 100;
+
+
+        if (recharge1 >= 1000) {
+            System.out.println(summary1 + bonus1);
+        } else {
+            if (recharge2 >= 1000)
+                System.out.println(summary2 + bonus2);
+        }
+
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+    }
+}
